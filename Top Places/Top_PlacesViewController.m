20688c5ca18d2404034e41f59fbede7e01a40957
amybear <amy@hyperfine.com>
2014-05-09 13:42:21 -0700

    Initial Commit

---
+++ Top Places/Top_PlacesViewController.m
@@ -0,0 +1,29 @@
+//
+//  Top_PlacesViewController.m
+//  Top Places
+//
+//  Created by Amy Bearman on 5/9/14.
+//  Copyright (c) 2014 Amy Bearman. All rights reserved.
+//
+
+#import "Top_PlacesViewController.h"
+
+@interface Top_PlacesViewController ()
+
+@end
+
+@implementation Top_PlacesViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
