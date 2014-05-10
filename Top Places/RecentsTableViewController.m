fb947288fd81cd763fe4bd9a8165fc0e1103453a
amybear <amy@hyperfine.com>
2014-05-10 14:09:46 -0700

    Nice, the model for Recents is properly updating now

--- Top Places/RecentsTableViewController.m
+++ Top Places/RecentsTableViewController.m
@@ -17,7 +17,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    NSLog(@"View appeared!");
     self.photos = [[NSUserDefaults standardUserDefaults] objectForKey:@"recentPhotos"];
+    [self.tableView reloadData];
 }
 
 @end
