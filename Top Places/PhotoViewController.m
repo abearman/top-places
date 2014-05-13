eef5d547a8495a4dfc7459407fac769691d45823
amybear <amy@hyperfine.com>
2014-05-12 18:20:44 -0700

    Downloading of an image now occurs on a separate thread

--- Top Places/PhotoViewController.m
+++ Top Places/PhotoViewController.m
@@ -8,15 +8,20 @@
 
 #import "PhotoViewController.h"
 
-@interface PhotoViewController () <UIScrollViewDelegate>
+@interface PhotoViewController () <UIScrollViewDelegate, UISplitViewControllerDelegate>
 @property (nonatomic, strong) UIImageView *imageView;
 @property (nonatomic) UIImage *image;
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
 @end
 
 @implementation PhotoViewController
 
 #pragma mark View Controller Lifecycle
+
+- (void)awakeFromNib {
+    self.splitViewController.delegate = self;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -59,16 +64,35 @@
     self.scrollView.zoomScale = 1.0;
     self.imageView.image = image;
     self.imageView.frame = CGRectMake(0, 0, image.size.width, image.size.height);
-    
     [self scaleImageToScrollView];
+    [self.spinner stopAnimating];
 }
 
 #pragma mark Public API
 
 - (void)setImageURL:(NSURL *)imageURL {
     _imageURL = imageURL;
-    NSData *imageData = [NSData dataWithContentsOfURL:imageURL];
-    self.image = [UIImage imageWithData:imageData];
+    [self startDownloadingImage];
+}
+
+- (void)startDownloadingImage {
+    self.image = nil;
+    if (self.imageURL) {
+        [self.spinner startAnimating];
+        NSURLRequest *request = [NSURLRequest requestWithURL:self.imageURL];
+        NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration ephemeralSessionConfiguration];
+        NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
+        NSURLSessionTask *task = [session downloadTaskWithRequest:request
+            completionHandler:^(NSURL *localfile, NSURLResponse *response, NSError *error) {
+                if (!error) {
+                    if ([request.URL isEqual:self.imageURL]) {
+                        UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:localfile]];
+                        dispatch_async(dispatch_get_main_queue(), ^{ self.image = image; });
+                    }
+                }
+        }];
+       [task resume];
+    }
 }
 
 #pragma mark Outlets
